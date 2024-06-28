e4175b5fac1249d18450c07ba4d572b348250df6
Nguyen Khoi Laptop <ngnguyenkhoib4@gmail.com>
2024-06-28 09:42:38 +0700

    adding js for resize type icon box home

--- wp-content/themes/sportswear/assets/js/app.js
+++ wp-content/themes/sportswear/assets/js/app.js
@@ -1,7 +1,29 @@
-jQuery(document).ready(function($){
-    $(document).on("click", ".coupon-toggle-btn", () => {
-        $(".cart-coupon").toggleClass("active");
-        $("body").toggleClass("overflow-hidden");
-      });
-      
-})
+jQuery(document).ready(function ($) {
+  $(document).on("click", ".coupon-toggle-btn", () => {
+    $(".cart-coupon").toggleClass("active");
+    $("body").toggleClass("overflow-hidden");
+  });
+
+  const windowsWidth = $(window).width();
+  const listiconFeature = $(".ss-type").find(".featured-box");
+  if (windowsWidth <= 576) {
+    const listHeight = [];
+    console.log(listiconFeature)
+    listiconFeature.each(function() {
+        listHeight.push($(this).height())
+    });
+    const maxHeight = arrayMax (listHeight);
+
+    listiconFeature.height(maxHeight)
+  }
+});
+
+function arrayMax(arr) {
+  var len = arr.length, max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
+};
